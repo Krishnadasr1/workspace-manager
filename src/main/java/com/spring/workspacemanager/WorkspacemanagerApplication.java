699488b7a0477f4100922382738f421b0a1e826e
krishnadas <krishnadasr2001@gmail.com>
2025-08-08 13:48:02 +0530

    initial commit

---
+++ src/main/java/com/spring/workspacemanager/WorkspacemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.workspacemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspacemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspacemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/workspacemanager/WorkspacemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.workspacemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspacemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
